296f3fc1add670aac574c471e601c4adb68ee4ed
vixitor <whe961@gmail.com>
2025-10-07 10:53:17 +0800

    update

--- scripts/run_ood_benchmark_vit.sh
+++ scripts/run_ood_benchmark_vit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 python tda_runner.py     --config configs \
                                                 --wandb-log \
-                                                --datasets I/A/V/R/S \
+                                                --datasets A \
                                                 --backbone ViT-B/16
